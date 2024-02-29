5d2fa7cd6acab676eefa8ca451418c94c4b78ebc
Alyona Redina <medvedeva_64@mail.ru>
2024-03-01 00:03:20 +0600

    Changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int calculate(double a, double b, double c) {
+    public int calculate(double amount, double percent, double time) {
 
-        double monthlyPercent = b / 12 / 100;
-        double coefficient = (monthlyPercent * (Math.pow(1 + monthlyPercent, c))) / (Math.pow(1 + monthlyPercent, c) - 1);
-        double result = coefficient * a;
+        double monthlyPercent = percent / 12 / 100;
+        double coefficient = (monthlyPercent * (Math.pow(1 + monthlyPercent, time))) / (Math.pow(1 + monthlyPercent, time) - 1);
+        double result = coefficient * amount;
 
         return (int) result;
 
